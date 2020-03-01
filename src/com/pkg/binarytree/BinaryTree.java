137cba796b6a24716bb4803a87dcc12bd089b5af
Just A Coder <54611515+just-a-coder@users.noreply.github.com>
2020-03-01 10:15:49 +0530

    level order traversal

--- src/com/pkg/binarytree/BinaryTree.java
+++ src/com/pkg/binarytree/BinaryTree.java
@@ -2,6 +2,9 @@ package com.pkg.binarytree;
 
 import com.pkg.linkedlist.Node;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinaryTree {
 
     public static int getHeight(Node root){
@@ -11,6 +14,46 @@ public class BinaryTree {
             int left = getHeight(root.left);
             int right = getHeight(root.right);
             return Math.max(left,right)+1;
+        }
+    }
+
+    // Prints level order traversal line
+    // by line using two queues.
+    static void levelOrder(Node root) {
+        if (root == null)
+            return;
+
+        Queue<Node> q = new LinkedList<>();
+
+        // Pushing root node into the queue.
+        q.add(root);
+
+        // Pushing delimiter into the queue.
+        q.add(null);
+
+        // Executing loop till queue becomes
+        // empty
+        while (!q.isEmpty()) {
+
+            Node curr = q.poll();
+
+            // condition to check the
+            // occurence of next level
+            if (curr == null) {
+                if (!q.isEmpty()) {
+                    q.add(null);
+                }
+            } else {
+                // Pushing left child current node
+                if (curr.left != null)
+                    q.add(curr.left);
+
+                // Pushing right child current node
+                if (curr.right != null)
+                    q.add(curr.right);
+
+                System.out.print(curr.data + " ");
+            }
         }
     }
 }
